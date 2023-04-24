ce9e44216fda3bc473f3aa4d6c568febb6a048dc
Pau-G <114681741+Pau-G@users.noreply.github.com>
2023-04-24 19:06:06 +0200

    first commit

--- CreateXmlFile.java
+++ CreateXmlFile.java
@@ -1,53 +1,52 @@
-import java.io.File;
 import java.io.FileWriter;
 import javax.xml.stream.XMLOutputFactory;
 import javax.xml.stream.XMLStreamWriter;
 
 public class CreateXmlFile {
 
-    public static void main(String[] args) {
+    public void createFile(String fileName, String node) {
 
         try {
             XMLOutputFactory factory = XMLOutputFactory.newInstance();
-            XMLStreamWriter writer = factory.createXMLStreamWriter(new FileWriter("cataleg.xml"));
+            XMLStreamWriter writer = factory.createXMLStreamWriter(new FileWriter(fileName + ".xml"));
 
             // Start document
             writer.writeStartDocument();
 
             // Start cataleg element
-            writer.writeStartElement("cataleg");
+            writer.writeStartElement(node);
 
-            // Start cd element
-            writer.writeStartElement("cd");
-            writer.writeAttribute("id", "1");
+            // // Start cd element
+            // writer.writeStartElement("cd");
+            // writer.writeAttribute("id", "1");
 
-            // titol element
-            writer.writeStartElement("titol");
-            writer.writeCharacters("Tinta Roja");
-            writer.writeEndElement();
+            // // titol element
+            // writer.writeStartElement("titol");
+            // writer.writeCharacters("Tinta Roja");
+            // writer.writeEndElement();
 
-            // artista element
-            writer.writeStartElement("artista");
-            writer.writeCharacters("Andres Calamaro");
-            writer.writeEndElement();
+            // // artista element
+            // writer.writeStartElement("artista");
+            // writer.writeCharacters("Andres Calamaro");
+            // writer.writeEndElement();
 
-            // pais element
-            writer.writeStartElement("pais");
-            writer.writeCharacters("Argentina");
-            writer.writeEndElement();
+            // // pais element
+            // writer.writeStartElement("pais");
+            // writer.writeCharacters("Argentina");
+            // writer.writeEndElement();
 
-            // preu element
-            writer.writeStartElement("preu");
-            writer.writeCharacters("10.90");
-            writer.writeEndElement();
+            // // preu element
+            // writer.writeStartElement("preu");
+            // writer.writeCharacters("10.90");
+            // writer.writeEndElement();
 
-            // any element
-            writer.writeStartElement("any");
-            writer.writeCharacters("2006");
-            writer.writeEndElement();
+            // // any element
+            // writer.writeStartElement("any");
+            // writer.writeCharacters("2006");
+            // writer.writeEndElement();
 
-            // End cd element
-            writer.writeEndElement();
+            // // End cd element
+            // writer.writeEndElement();
 
             // End cataleg element
             writer.writeEndElement();
