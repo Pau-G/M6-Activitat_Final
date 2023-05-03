773cc5386c400b8cc70c9f171ff86eafdc7e7f63
DNolberczyk <nolberczyk460@gmail.com>
2023-05-03 17:04:37 +0200

    Update 02_05_2023

--- m6-activitat-final/src/DatabaseConnection.java
+++ m6-activitat-final/src/DatabaseConnection.java
@@ -1,4 +1,6 @@
 import java.io.File;
+import java.io.PrintStream;
+import java.util.Collection;
 
 import javax.xml.crypto.Data;
 
@@ -6,11 +8,12 @@ import org.xmldb.api.*;
 import org.xmldb.api.base.*;
 import org.xmldb.api.modules.*;
 
-public class DatabaseConnection implements IDatabaseConnection{
+public class DatabaseConnection implements IDatabaseConnection {
 
 	public static DatabaseConnection instance;
 
-	private DatabaseConnection() {}
+	private DatabaseConnection() {
+	}
 
 	public static DatabaseConnection getInstance() {
 		if (instance == null) {
@@ -21,17 +24,17 @@ public class DatabaseConnection implements IDatabaseConnection{
 
 	public Database database;
 
-	public void connect() throws XMLDBException{
+	public void connect() throws XMLDBException {
 
 		String driver = "org.exist.xmldb.DatabaseImpl"; // Driver para eXist
-		Collection col = null; // Colecci�n
+		org.xmldb.api.base.Collection col = null; // Colecci�n
 		String URI = "xmldb:exist://localhost:8080/exist/xmlrpc/db/"; // URI colecci�n
 		String usu = "admin"; // Usuario
 		String usuPwd = "2003"; // Clave
 
 		try {
 			Class cl = Class.forName(driver); // Cargar del driver
-			database = (Database) cl.getDeclaredConstructor().newInstance(); // Instancia de la BD
+			Database database = (Database) cl.newInstance(); // Instancia de la BD
 			DatabaseManager.registerDatabase(database); // Registro del driver
 
 		} catch (Exception e) {
@@ -39,28 +42,24 @@ public class DatabaseConnection implements IDatabaseConnection{
 			e.printStackTrace();
 		}
 
-		// col = DatabaseManager.getCollection(URI, usu, usuPwd); // Connexi� amb la nostra col�lecci�
+		col = DatabaseManager.getCollection(URI, usu, usuPwd); // Connexi� amb la nostra col�lecci�
 
-		// if (col == null)
-		// 	System.out.println(" *** LA COLECCION NO EXISTE. ***");
+		if (col == null)
+			System.out.println(" *** LA COLECCION NO EXISTE. ***");
 
-		// // Exemple de consulta XQuery
+		// Exemple de consulta XQuery
 
-		// XPathQueryService servicio = (XPathQueryService) col.getService("XPathQueryService", "1.0");
+		XPathQueryService servicio = (XPathQueryService) col.getService("XPathQueryService", "1.0");
 
-		// // Ara farem i executarem la consulta xquery sobre el nostre fitxer
-		// ResourceSet result = servicio.query("for $a in //title/text() return $a");
+		// Ara farem i executarem la consulta xquery sobre el nostre fitxer
+		ResourceSet result = servicio.query("for $a in //title/text() return $a");
 
-		// // Exemple de com mostrar els resultats de la nostra consulta.
-		// ResourceIterator i;
-		// i = result.getIterator();
-		// if (!i.hasMoreResources())
-		// 	System.out.println(" LA CONSULTA NO DEVUELVE NADA.");
+		// Exemple de com mostrar els resultats de la nostra consulta.
+		ResourceIterator i;
+		i = result.getIterator();
+		if (!i.hasMoreResources())
+			System.out.println(" LA CONSULTA NO DEVUELVE NADA.");
 
-		// // while (i.hasMoreResources()) {
-		// // 	Resource r = i.nextResource();
-		// // 	System.out.println((String) r.getContent());
-		// // }
-		// col.close(); // Cerramos coleccion
+		col.close(); // Cerramos coleccion
 	}
 }
