f218465646e4a189b1a179e3d0403fd7a51f6dba
DNolberczyk <nolberczyk460@gmail.com>
2023-05-08 16:34:38 +0200

    Hello

--- m6-activitat-final/src/ExistHelper.java
+++ m6-activitat-final/src/ExistHelper.java
@@ -18,7 +18,8 @@ public class ExistHelper {
         databaseConnection.connect();
     }
 
-    private void queryToCollectionFile(org.xmldb.api.base.Collection col, String fileName, String query) throws XMLDBException {
+    private void queryToCollectionFile(org.xmldb.api.base.Collection col, String fileName, String query)
+            throws XMLDBException {
         XPathQueryService servicio = (XPathQueryService) col.getService("XPathQueryService", "1.0");
         ResourceSet result = servicio.query(query);
 
@@ -30,12 +31,12 @@ public class ExistHelper {
         while (i.hasMoreResources()) {
             Resource r = i.nextResource();
             System.out.println((String) r.getContent());
-        } 
+        }
     }
 
     public void createCollection(String collectionname) throws XMLDBException {
         boolean result = databaseConnection.createCollection(collectionname);
-        if (result){
+        if (result) {
             System.out.println("Col·leció creada");
         } else {
             System.out.println("Error al crear la col·lecció");
@@ -73,33 +74,35 @@ public class ExistHelper {
         org.xmldb.api.base.Collection col = databaseConnection.getCollection(collectionName);
 
         File arxiu = new File(fileName);
-        if(!arxiu.canRead()) {
+        if (!arxiu.canRead()) {
             System.out.println("ERROR AL LLEIGIR ARXIU");
         } else {
             org.xmldb.api.base.Resource nouRecurs = col.createResource(arxiu.getName(), "XMLResource");
-            nouRecurs.setContent(arxiu); //Comprova que es un arxiu
-            col.storeResource(nouRecurs); 
+            nouRecurs.setContent(arxiu); // Comprova que es un arxiu
+            col.storeResource(nouRecurs);
         }
     }
 
-    public void insertElement(String collectionName, String fileName, String titol, String artistName, String countryName, float price, int year) throws XMLDBException {
+    public void insertElement(String collectionName, String fileName, String titol, String artistName,
+            String countryName, float price, int year) throws XMLDBException {
         // Desem la coleció desitjada
         org.xmldb.api.base.Collection col = databaseConnection.getCollection(collectionName);
 
-        String query = "update insert" + 
-            "<cd id='" + autoincrement + "'>" + 
+        String query = "update insert" +
+                "<cd id='" + autoincrement + "'>" +
                 "<titol>" + titol + "</titol>" +
                 "<artista>" + artistName + "</artista>" +
                 "<pais>" + countryName + "</pais>" +
                 "<preu>" + price + "</price>" +
                 "<any>" + year + "</any>" +
-            "</cd>" +
-            "into doc('" + fileName + "')//cataleg";
+                "</cd>" +
+                "into doc('" + fileName + "')//cataleg";
 
         queryToCollectionFile(col, fileName, query);
     }
 
-    public void changeCDPrice(String collectionName, String fileName, String cdTitle, float price) throws XMLDBException {
+    public void changeCDPrice(String collectionName, String fileName, String cdTitle, float price)
+            throws XMLDBException {
         // Desem la coleció desitjada
         org.xmldb.api.base.Collection col = databaseConnection.getCollection(collectionName);
 
@@ -112,19 +115,27 @@ public class ExistHelper {
         org.xmldb.api.base.Collection col = databaseConnection.getCollection(collectionName);
 
         String query = "<NumTotalCDs>{count(/cataleg/cd)}</NumTotalCDs>";
-        
+
         queryToCollectionFile(col, fileName, query);
     }
 
     public void deleteCD(String collectionName, String fileName, String cdTitle) throws XMLDBException {
         org.xmldb.api.base.Collection col = databaseConnection.getCollection(collectionName);
 
-        String query = "/cataleg/cd[titol=" + cdTitle + "]";
-        
+        String query = "let $titol :=" + cdTitle + "for $cd in /cataleg/cd" +
+        "where $cd/titol = $titol" +
+        "return delete node $cd";
+
         queryToCollectionFile(col, fileName, query);
     }
 
-    public void getPreviousCDs(int year) {
+    public void getPreviousCDs(String collectionName, String fileName, int year) throws XMLDBException {
+          org.xmldb.api.base.Collection col = databaseConnection.getCollection(collectionName);
+
+        String query = "for $cd in /cataleg/cd" +
+        "where $cd/any <"+ year +
+        "return concat('Títol: ', $cd/titol, ', Artista: ', $cd/artista)";
         
+        queryToCollectionFile(col, fileName, query);
     }
 }
